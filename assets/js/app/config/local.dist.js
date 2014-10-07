ce8594dca73eba64617c55ceefcc03cc178073ed
Eric Nishio <eric.nishio@nordsoftware.com>
2014-10-07 09:24:16 +0300

    Configure default base API URLs

--- assets/js/app/config/local.dist.js
+++ assets/js/app/config/local.dist.js
@@ -2,8 +2,8 @@
 // TODO: Copy this file and name it local.js
 angular.module('Gapminder').factory('LocalConfig', function() {
     return {
-        //baseApiUrl: 'http://localhost:1337/api',
-        //baseMockApiUrl: 'http://localhost:1337/mockApi',
+        //baseApiUrl: 'http://gapminder.org/api',
+        //baseMockApiUrl: 'http://localhost:1338/api',
         /*apiMocks: {
             // API URIs that are mocked for development purposes.
             GET: [],
--- assets/js/app/config/main.js
+++ assets/js/app/config/main.js
@@ -1,8 +1,8 @@
 angular.module('Gapminder').factory('MainConfig', function() {
     return {
         baseUrl: 'http://localhost:1337',
-        baseApiUrl: 'http://localhost:1337/api',
-        baseMockApiUrl: 'http://localhost:1337/mockApi',
+        baseApiUrl: 'http://gapminder.org/api',
+        baseMockApiUrl: 'http://localhost:1338/api',
         apiMocks: {
             // API URIs that are mocked for development purposes.
             GET: [],
