ca1f207c69fadaa42f714117aa3380ef11ba11cd
Eric Nishio <eric.nishio@nordsoftware.com>
2014-10-29 13:06:06 +0200

    Auto-compile index.html when changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -329,6 +329,8 @@ module.exports = function(grunt) {
                     '<%= paths.vendor %>/**'
                 ],
                 tasks: [
+                    'template:development',
+                    'copy:other',
                     'sync:main'
                 ]
             }
