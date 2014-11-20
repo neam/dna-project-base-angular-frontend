bf882dfde30f94ea9519cc89129b8838a8870e27
Eric Nishio <eric.nishio@nordsoftware.com>
2014-11-20 11:29:18 +0200

    Support node IDs and routes as custom page params

--- src/js/services/CustomPageService.js
+++ src/js/services/CustomPageService.js
@@ -11,26 +11,6 @@ function(
 ) {
     var item;
 
-    /**
-     * Loads a page item by route (ID).
-     * @param {string} route
-     * @returns {Deferred.promise}
-     */
-    function loadPage(route) {
-        var dfd = $q.defer(),
-            apiUrl = '/item/:route'.replace(':route', $window.encodeURIComponent(route));
-
-        $http.get(ApiService.getApiUrl(apiUrl))
-            .success(function(page) {
-                dfd.resolve(page);
-            })
-            .error(function(err) {
-                dfd.reject(err);
-            });
-
-        return dfd.promise;
-    }
-
     return {
         /**
          * Initializes the service.
@@ -40,12 +20,49 @@ function(
         init: function(route) {
             var dfd = $q.defer();
 
-            loadPage(route).then(function(page) {
+            this.loadPage(route).then(function(page) {
                 item = page;
                 dfd.resolve(page);
             }, function(err) {
                 dfd.reject(err);
             });
+
+            return dfd.promise;
+        },
+
+        /**
+         * Returns an API URL to the given page.
+         * @param {string|number} identifier
+         * @returns {string}
+         */
+        getPageApiUrl: function(identifier) {
+            var identifierAsNumber = _.isString(identifier)
+                    ? identifier.replace('/', '')
+                    : identifier;
+
+            var url = identifierAsNumber > 0 // check if the identifier is a number
+                    ? identifierAsNumber
+                    : $window.encodeURIComponent(identifier);
+
+            return ApiService.getApiUrl('/item/' + url);
+        },
+
+        /**
+         * Loads a page item by route (ID).
+         * @param {string} route
+         * @returns {Deferred.promise}
+         */
+        loadPage: function(route) {
+            var dfd = $q.defer(),
+                apiUrl = this.getPageApiUrl(route);
+
+            $http.get(apiUrl)
+                .success(function(page) {
+                    dfd.resolve(page);
+                })
+                .error(function(err) {
+                    dfd.reject(err);
+                });
 
             return dfd.promise;
         }
