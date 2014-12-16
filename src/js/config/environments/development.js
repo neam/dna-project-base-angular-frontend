f7fec7555bb3e3972134188b573e1791a2fddbc4
Eric Nishio <eric.nishio@nordsoftware.com>
2014-12-16 13:50:38 +0200

    Revert navbar links

--- src/js/config/environments/development.js
+++ src/js/config/environments/development.js
@@ -8,91 +8,29 @@ angular.module('Gapminder').factory('EnvironmentConfig', [function() {
             "children": [
                 {
                     "node_id": null,
-                    "menu_label": "Facts",
-                    "caption": null,
+                    "menu_label": "Health",
+                    "caption": "About health.",
                     "url": null,
                     "children": [
                         {
-                            "node_id": null,
-                            "menu_label": "Gapminder World",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/world"
-                        },
-                        {
-                            "node_id": null,
-                            "menu_label": "Answers",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/1308"
-                        },
-                        {
-                            "node_id": null,
-                            "menu_label": "Ignorance Project",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/ignorance"
-                        },
-                        {
-                            "node_id": null,
-                            "menu_label": "Data",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/data"
+                            "node_id": 3,
+                            "menu_label": "Ebola",
+                            "caption": "Most people need more money to lead a good life.",
+                            "url": "/ebola"
                         }
                     ]
                 },
                 {
                     "node_id": null,
-                    "menu_label": "Teach",
+                    "menu_label": "Exercises",
                     "caption": null,
                     "url": null,
                     "children": [
                         {
-                            "node_id": null,
-                            "menu_label": "For Teachers",
+                            "node_id": 4,
+                            "menu_label": "Ebola Dashboard",
                             "caption": null,
-                            "url": "http://www.gapminder.org/for-teachers"
-                        },
-                        {
-                            "node_id": null,
-                            "menu_label": "Slideshows",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/presentations"
-                        },
-                        {
-                            "node_id": null,
-                            "menu_label": "Workshops",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/workshops"
-                        },
-                        {
-                            "node_id": null,
-                            "menu_label": "Test Questions",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/test-questions"
-                        }
-                    ]
-                },
-                {
-                    "node_id": null,
-                    "menu_label": "About",
-                    "caption": null,
-                    "url": null,
-                    "children": [
-                        {
-                            "node_id": null,
-                            "menu_label": "Our Organization",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/about-gapminder"
-                        },
-                        {
-                            "node_id": null,
-                            "menu_label": "Blog",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/news"
-                        },
-                        {
-                            "node_id": null,
-                            "menu_label": "FAQ",
-                            "caption": null,
-                            "url": "http://www.gapminder.org/faq_frequently_asked_questions"
+                            "url": "/ebola/dashboard"
                         }
                     ]
                 }
