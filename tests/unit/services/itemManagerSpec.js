6ba6c60b7b6a140a72bb9fe714d6b6d7580f958e
Eric Nishio <eric.nishio@nordsoftware.com>
2015-01-12 15:52:58 +0200

    Write unit tests for itemManager

--- tests/unit/services/itemManagerSpec.js
+++ tests/unit/services/itemManagerSpec.js
@@ -1,5 +1,6 @@
 describe('itemManager', function() {
-  var $location,
+  var $rootScope,
+      $location,
       $http,
       $httpBackend,
       api,
@@ -8,7 +9,8 @@ describe('itemManager', function() {
 
   beforeEach(module('Gapminder'));
 
-  beforeEach(inject(function(_$location_, _$http_, _$httpBackend_, _api_, _itemManager_, _urlManager_) {
+  beforeEach(inject(function(_$rootScope_, _$location_, _$http_, _$httpBackend_, _api_, _itemManager_, _urlManager_) {
+    $rootScope = _$rootScope_;
     $location = _$location_;
     $http = _$http_;
     $httpBackend = _$httpBackend_;
@@ -81,6 +83,123 @@ describe('itemManager', function() {
 
   it('should return a page API URL with a node ID (type string)', function() {
     expect(itemManager.getPageApiUrl('100')).toBe('http://localhost:1338/api/item/100');
+  });
+
+  it('should get an item route param from the URL', function() {
+    setLocation('localhost', '', '/ebola');
+    expect(itemManager.getItemRouteParamFromUrl()).toBe('/ebola');
+  });
+
+  it('should get an item identifier from the URL', function() {
+    setLocation('localhost', '', '/exercises/100');
+    expect(itemManager.getItemIdentifierFromUrl()).toBe('http://localhost:1338/api/item/100'); // TODO: Is this correct?
+  });
+
+  it('should create a user profile URL', function() {
+    expect(itemManager.createUserProfileUrl(2)).toBe('http://www.gapminder.org/profiles/2');
+  });
+
+  it('should create an item URL for a composition with a defined URL', function() {
+    var composition = {
+      url: 'http://www.gapminder.org/exercises/100'
+    };
+
+    expect(itemManager.createItemUrl(composition)).toBe('http://www.gapminder.org/exercises/100');
+  });
+
+  it('should create an item URL for a composition with an undefined URL', function() {
+    var composition = {
+      node_id: 1092
+    };
+
+    expect(itemManager.createItemUrl(composition)).toBe('#/1092');
+  });
+
+  it('should navigate to a profile page', function() {
+    var userId = 1,
+        fakeEvent = {
+          preventDefault: function() {
+            return true;
+          }
+        };
+
+    spyOn(urlManager, 'redirect');
+    itemManager.goToProfile(userId, fakeEvent);
+    expect(urlManager.redirect).toHaveBeenCalled();
+  });
+
+  it('should check if an item has contributors', function() {
+    var item = {
+      contributors: [
+        {user_id: 1},
+        {user_id: 2}
+      ]
+    };
+
+    expect(itemManager.itemHasContributors(item)).toBeTruthy();
+  });
+
+  it('should check if an item does not have contributors (null)', function() {
+    var item = {
+      contributors: null
+    };
+
+    expect(itemManager.itemHasContributors(item)).toBeFalsy();
+  });
+
+  it('should check if an item does not have contributors (empty array)', function() {
+    var item = {
+      contributors: []
+    };
+
+    expect(itemManager.itemHasContributors(item)).toBeFalsy();
+  });
+
+  it('should remove an admin user from a contributor array', function() {
+    var item = {
+        contributors: [
+          {
+            user_id: 1
+          }
+        ]
+      },
+      containsAdmin = false;
+
+    itemManager.removeAdminContributorFromItem(item);
+
+    angular.forEach(item.contributors, function(contributor) {
+      if (!containsAdmin && contributor.user_id === 1) {
+        containsAdmin = true;
+      }
+    });
+
+    expect(containsAdmin).toBeFalsy();
+  });
+
+  it('should set the meta description tag to item about', function() {
+    var description = 'This is an item.',
+        item = {
+          attributes: {
+            about: description
+          }
+        };
+
+    itemManager.setMetaDescriptionFromItem(item);
+
+    expect($rootScope.metaDescription).toBe(description);
+  });
+
+  it('should set the meta description tag to item subheading', function() {
+    var description = 'This is an item.',
+        item = {
+          attributes: {
+            subheading: description
+          }
+        };
+
+    itemManager.setMetaDescriptionFromItem(item);
+
+    expect($rootScope.metaDescription).toBe(description);
   });
 
   /**
