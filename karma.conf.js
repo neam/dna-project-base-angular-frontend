fee31bbd0bf743dd47228a572435d520081a87e5
Eric Nishio <eric.nishio@nordsoftware.com>
2015-01-12 15:06:55 +0200

    Set up karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -29,13 +29,19 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'dist/templates/**/*.html': ['ng-html2js']
+      'dist/templates/**/*.html': ['ng-html2js'],
+      'dist/assets/js/main.js': ['coverage']
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    // coverage reporter
+    coverageReporter: {
+      type: 'text-summary'
+    },
 
     // web server port
     port: 9876,
