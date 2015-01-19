44adf78ab6497e6872f45227c5e23f2a213f736f
Fredrik Wollsén <fredrik@neam.se>
2015-01-20 00:51:07 +0200

    Restored grunt file syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,10 @@ module.exports = function(grunt) {
     data: {
       BASE_API_URL_DEVELOPMENT: grunt.option('api') || 'http://localhost:1338/api',
       BASE_API_URL_PRODUCTION: grunt.option('api') || 'http://cms.gapminder.org/api/v1',
-      BASE_API_URL_STAGE: grunt.option('api') || 'http://cms.gapminder.org/api/v1'
+      BASE_API_URL_STAGE: grunt.option('api') || 'http://cms.gapminder.org/api/v1',
       MIXPANEL_ID_DEVELOPMENT: grunt.option('mixpanelId') || '',
       MIXPANEL_ID_PRODUCTION: grunt.option('mixpanelId') || '',
-      MIXPANEL_ID_STAGE: grunt.option('mixpanelId') || '',
+      MIXPANEL_ID_STAGE: grunt.option('mixpanelId') || ''
     }
   });
 
