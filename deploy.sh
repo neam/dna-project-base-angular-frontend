bc8f2b9244b51db489d8fd34b541e87423377039
Fredrik Wollsén <fredrik@neam.se>
2015-01-24 01:16:34 +0100

    Generating env.js in dist dir as to not publish with mock api setting

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,8 @@ acl_public = True" > /tmp/.$S3_BUCKET-s3.s3cfg
 export PAGES_S3_TARGET=$DEPLOYMENT_DIR
 
 # Run erb to generate the published config file
-erb fork-src/js/env.js.erb > fork-src/js/env.js
+erb dist/js/env.js.erb > dist/js/env.js
+rm dist/js/env.js.erb
 
 # Upload to S3
 s3cmd -v --config=/tmp/.$S3_BUCKET-s3.s3cfg --acl-public --recursive sync dist/ "$PAGES_S3_TARGET/"
