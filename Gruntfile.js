4dec4de3c8eb5bd6c362a37a3966cdfdc9226ff9
Fredrik Wollsén <fredrik@neam.se>
2015-01-26 02:29:21 +0100

    Just one, not three, grunt variables for mixpanel project token

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,7 @@ module.exports = function(grunt) {
       BASE_API_URL_PRODUCTION: grunt.option('api') || 'http://cms.gapminder.org/api/v1',
       BASE_API_URL_STAGE: grunt.option('api') || 'http://cms.gapminder.org/api/v1',
       DNA_PATH: '../angular-frontend-dna',
-      MIXPANEL_PROJECT_TOKEN_DEVELOPMENT: grunt.option('mixpanelId') || '',
-      MIXPANEL_PROJECT_TOKEN_PRODUCTION: grunt.option('mixpanelId') || '',
-      MIXPANEL_PROJECT_TOKEN_STAGE: grunt.option('mixpanelId') || ''
+      MIXPANEL_PROJECT_TOKEN: grunt.option('mixpanelId') || '',
     }
   });
 
