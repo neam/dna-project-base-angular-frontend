da55321f1eb7c8a8e7547a72ab4fedb2caea1413
Fredrik Wollsén <fredrik@neam.se>
2015-03-03 10:31:42 +0200

    More example views from latest inspinia theme, including sample views with top navigation

--- dna-boilerplate/app/scripts/config.js
+++ dna-boilerplate/app/scripts/config.js
@@ -3,12 +3,46 @@
  * Each view is defined as state.
  */
 function config($stateProvider, $urlRouterProvider) {
-    //$urlRouterProvider.otherwise("/user/login");
+    $urlRouterProvider.otherwise("/");
     $stateProvider
 
-        .state('example-view', {
+        .state('root', {
+            abstract: true,
+            url: "",
+            templateUrl: "views/common/content.html",
+        })
+        .state('root.main', {
+            url: "/",
+            templateUrl: "views/main.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('root.minor', {
+            url: "/minor",
+            templateUrl: "views/minor.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('topnav', {
+            abstract: true,
+            url: "/topnav",
+            templateUrl: "views/common/content_top_navigation.html",
+        })
+        .state('topnav.main', {
+            url: "/",
+            templateUrl: "views/main.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('topnav.minor', {
+            url: "/minor",
+            templateUrl: "views/minor.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('root.example-view', {
             url: "/example-view",
-            //templateUrl: "views/example-view.html",
+            templateUrl: "views/example-view.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('root.pre-launch', {
+            url: "/pre-launch",
             templateUrl: "views/pre-launch.html",
             data: { pageTitle: 'Example view' }
         })
