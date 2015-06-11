42cdc452e6ef330eb2c3319c5a03b92198cf3442
Fredrik Wollsén <fredrik@neam.se>
2015-06-11 14:30:56 +0200

    Synced improvements from dna projects (handsontable, auth-init, ngResource, tweaks)

--- dna-boilerplate/app/scripts/app.js
+++ dna-boilerplate/app/scripts/app.js
@@ -2,4 +2,44 @@
     var app = angular.module('app', [
         'angular-frontend'
     ]);
+
+    app.factory('metadataService', function ($http) {
+
+        var factory = {};
+
+        factory.getMetadataPromise = function () {
+            return $http({
+                url: env.API_BASE_URL + '/v0/metadata',
+                method: 'GET'
+            });
+        };
+
+        return factory;
+
+    });
+
+    app.factory('authInterceptor', function ($rootScope, $q, UserApp) {
+        return {
+            request: function (config) {
+                config.headers = config.headers || {};
+                if (UserApp.tokenStorage.get()) {
+                    //config.headers.Authorization = 'UserappToken ' + UserApp.tokenStorage.get();
+                }
+                config.withCredentials = true;
+                return config;
+            },
+            response: function (response) {
+                if (response.status === 401) {
+                    // handle the case where the user is not authenticated
+                    console.log('unauthorized request intercepted by authInterceptor');
+                }
+                return response || $q.when(response);
+            }
+        };
+    });
+
+    app.config(function ($httpProvider) {
+        $httpProvider.interceptors.push('authInterceptor');
+    });
+
 })();
--- dna-boilerplate/app/scripts/config.js
+++ dna-boilerplate/app/scripts/config.js
@@ -1,61 +1,110 @@
-/**
- * App uses AngularUI Router to manage routing and views
- * Each view is defined as state.
- */
-function config($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
-    $stateProvider
+(function () {
 
-        .state('root', {
-            abstract: true,
-            url: "",
-            templateUrl: "views/common/content.html",
-        })
-        .state('root.main', {
-            url: "/",
-            templateUrl: "views/main.html",
-            data: { pageTitle: 'Example view' }
-        })
-        .state('root.minor', {
-            url: "/minor",
-            templateUrl: "views/minor.html",
-            data: { pageTitle: 'Example view' }
-        })
-        .state('topnav', {
-            abstract: true,
-            url: "/topnav",
-            templateUrl: "views/common/content_top_navigation.html",
-        })
-        .state('topnav.main', {
-            url: "/",
-            templateUrl: "views/main.html",
-            data: { pageTitle: 'Example view' }
-        })
-        .state('topnav.minor', {
-            url: "/minor",
-            templateUrl: "views/minor.html",
-            data: { pageTitle: 'Example view' }
-        })
-        .state('root.example-view', {
-            url: "/example-view",
-            templateUrl: "views/example-view.html",
-            data: { pageTitle: 'Example view' }
-        })
-        .state('root.pre-launch', {
-            url: "/pre-launch",
-            templateUrl: "views/pre-launch.html",
-            data: { pageTitle: 'Example view' }
-        })
+    /**
+     * App uses AngularUI Router to manage routing and views
+     * Each view is defined as state.
+     */
+    function config($stateProvider, $urlRouterProvider, $provide) {
+        $urlRouterProvider.otherwise("/");
+        $stateProvider
 
-    ;
-}
-angular
-    .module('app')
-    .config(config)
-    .run(function ($rootScope, $state, user) {
+            .state('root', {
+                abstract: true,
+                url: "",
+                templateUrl: "views/common/content.html",
+                resolve: {
+                    // this section ensures that metadata has been queried and is available before rendering any root child-state
+                    metadataService: 'metadataService',
+                    metadata: function (metadataService) {
+                        return metadataService.getMetadataPromise();
+                    }
+                }
+            })
+            .state('root.main', {
+                url: "/",
+                templateUrl: "views/main.html",
+                data: { pageTitle: 'Example view' }
+            })
+            .state('root.minor', {
+                url: "/minor",
+                templateUrl: "views/minor.html",
+                data: { pageTitle: 'Example view' }
+            })
+            .state('topnav', {
+                abstract: true,
+                url: "/topnav",
+                templateUrl: "views/common/content_top_navigation.html",
+            })
+            .state('topnav.main', {
+                url: "/",
+                templateUrl: "views/main.html",
+                data: { pageTitle: 'Example view' }
+            })
+            .state('topnav.minor', {
+                url: "/minor",
+                templateUrl: "views/minor.html",
+                data: { pageTitle: 'Example view' }
+            })
+            .state('root.example-view', {
+                url: "/example-view",
+                templateUrl: "views/example-view.html",
+                resolve: {
+                    // Inject the metadata resolved in the root state
+                    metadata: function (metadata) {
+                        return metadata;
+                    }
+                },
+                data: { pageTitle: 'Example view' }
+            })
+            .state('root.pre-launch', {
+                url: "/pre-launch",
+                templateUrl: "views/pre-launch.html",
+                data: { pageTitle: 'Example view' }
+            })
 
-        $rootScope.$on('user.login', function () {
-            //$state.go('example-view');
+        ;
+    }
+
+    angular
+        .module('app')
+        .config(config)
+        .run(function ($rootScope, $state, user, $http, UserApp) {
+
+            $rootScope.$on('user.login', function () {
+                //$state.go('report-time');
+
+                // update ua_session_token for rest-api so that api requests are authenticated using the same userapp user
+                // (this is a workaround for the fact that cookies are not shared across domains)
+                $http.post(env.API_BASE_URL + '/v0/auth/loginNotify', {
+                    token: UserApp.tokenStorage.get(),
+                    user: user
+                })
+                    .success(function (data, status, headers, config) {
+                        console.log('login rest api sync request successful');
+                    })
+                    .error(function (data, status, headers, config) {
+                        console.log('login rest api sync request not successful');
+                    });
+
+            });
+
+            $rootScope.$on('user.logout', function () {
+
+                // destroy session also on rest-api so that api requests are no longer authenticated using the userapp user that was logged out
+                // (this is a workaround for the fact that cookies are not shared across domains)
+                $http.post(env.API_BASE_URL + '/v0/auth/logoutNotify', {
+                    token: UserApp.tokenStorage.get(),
+                    user: user
+                })
+                    .success(function (data, status, headers, config) {
+                        console.log('logout rest api sync request successful');
+                    })
+                    .error(function (data, status, headers, config) {
+                        console.log('logout rest api sync request not successful');
+                    });
+
+            });
+
         });
 
-    });
+})();
