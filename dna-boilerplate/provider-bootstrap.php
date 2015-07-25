293e3859ab42d3ba86bfff5f6549f619c8fe98cd
Fredrik Wollsén <fredrik@neam.se>
2015-07-25 21:12:18 +0300

    Boilerplate enhancements necessary for has-one-relations select2-editor usage

--- dna-boilerplate/provider-bootstrap.php
+++ dna-boilerplate/provider-bootstrap.php
@@ -7,9 +7,28 @@
 
 $handsontableOrdinaryColumn = function ($attribute, $model) {
     $attribute = str_replace("hot-column.", "", $attribute);
-    return <<<INPUT
-    <hot-column data="attributes.$attribute" title="'$attribute'"></hot-column>
+
+    $itemTypeAttributes = $model->itemTypeAttributes();
+    $attributeInfo = $itemTypeAttributes[$attribute];
+
+    switch ($attributeInfo["type"]) {
+        default:
+            return "    <!-- \"$attribute\" TYPE {$attributeInfo["type"]} TODO -->";
+            break;
+        case "primary-key":
+        case "ordinary":
+            return <<<INPUT
+    <hot-column data="attributes.$attribute" title="'$attribute'" attribute-type="'{$attributeInfo["type"]}'"></hot-column>
 INPUT;
+            break;
+        case "has-one-relation":
+            $lcfirstModelClass = lcfirst(get_class($model));
+            return <<<INPUT
+    <hot-column data="attributes.$attribute.id" title="'$attribute'" attribute-type="'has-one-relation'" renderer="{$lcfirstModelClass}Crud.handsontable.columnLogic.$attribute.cellRenderer" editor="'select2'" select2Options="{$lcfirstModelClass}Crud.handsontable.columnLogic.$attribute.select2Options"></hot-column>
+INPUT;
+            break;
+    }
+
 };
 
 $handsontableCheckboxColumn = function ($attribute, $model) {
