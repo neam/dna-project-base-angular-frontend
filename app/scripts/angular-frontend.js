01b34ff427e56e67c729cdace2eeba4802f03e15
Fredrik Wollsén <fredrik@neam.se>
2015-08-28 10:52:02 +0200

    Added general modal controller

--- app/scripts/angular-frontend.js
+++ app/scripts/angular-frontend.js
@@ -115,4 +115,51 @@
 
     });
 
+    /**
+     * open(template, size)
+     * for instance:
+     * open('modals/foo.html', 'lg')
+     */
+    app.controller('GeneralModalController', function ($scope, $modal, $log) {
+
+        $scope.open = function (template, size) {
+
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: template,
+                controller: 'GeneralModalInstanceController',
+                size: size
+                /*
+                 resolve: {
+                 items: function () {
+                 return $scope.items;
+                 }
+                 }
+                 */
+            });
+
+            modalInstance.result.then(function (/*selectedItem*/) {
+                //$scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+    });
+
+    // Please note that $modalInstance represents a modal window (instance) dependency.
+    // It is not the same as the $modal service used above.
+    app.controller('GeneralModalInstanceController', function ($scope, $modalInstance /*, items */) {
+
+        //$scope.items = items;
+
+        $scope.ok = function () {
+            $modalInstance.close(/*$scope.selected.item*/);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
+
 })();
