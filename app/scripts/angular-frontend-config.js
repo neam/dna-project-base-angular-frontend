e7dcd7c0a8b5fe8156c0a2f1acb3e809a61f2535
Fredrik Wollsén <fredrik@neam.se>
2015-11-03 01:21:40 +0000

    Moved google chart config to angular-frontend + Angular google chart upgrade 0.0.11 -> 0.1.0-beta.1

--- app/scripts/angular-frontend-config.js
+++ app/scripts/angular-frontend-config.js
@@ -564,3 +564,13 @@ angular
         });
 
     });
+
+(function(){
+    angular.module('googlechart')
+        .value('googleChartApiConfig', {
+            version: '1.1',
+            optionalSettings: {
+                packages: ['corechart', 'sankey', 'calendar']
+            }
+        });
+})();
