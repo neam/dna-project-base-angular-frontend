b302d1276881340a99cc1eef385262805596d620
Fredrik Wollsén <fredrik@neam.se>
2015-11-21 18:43:58 +0200

    Added filesize angular filter

--- app/scripts/angular-frontend-filters.js
+++ app/scripts/angular-frontend-filters.js
@@ -12,4 +12,38 @@ angular.module('angular-frontend-filters', [])
         }
     })
 
+    /**
+     * Filesize Filter
+     * @Param length, default is 0
+     * @return string
+     */
+    .filter('filesize', function () {
+        return function (size) {
+            if (isNaN(size))
+                size = 0;
+
+            if (size < 1024)
+                return size + ' Bytes';
+
+            size /= 1024;
+
+            if (size < 1024)
+                return size.toFixed(2) + ' Kb';
+
+            size /= 1024;
+
+            if (size < 1024)
+                return size.toFixed(2) + ' Mb';
+
+            size /= 1024;
+
+            if (size < 1024)
+                return size.toFixed(2) + ' Gb';
+
+            size /= 1024;
+
+            return size.toFixed(2) + ' Tb';
+        };
+    });
+
 ;
