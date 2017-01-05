60b28f4c38ed1a98e2e033ed44e2290f9e50e4ac
Fredrik Wollsén <fredrik@neam.se>
2017-01-05 11:24:30 +0200

    Updated already versioned deps to those included in inspinia 2.6

--- app/scripts/inspinia/plugins/fullcalendar/fullcalendar.js
+++ app/scripts/inspinia/plugins/fullcalendar/fullcalendar.js
@@ -6,7 +6,7 @@
 
 (function(factory) {
 	if (typeof define === 'function' && define.amd) {
-		define([ '../../../../../../../.', 'moment' ], factory);
+		define([ 'jquery', 'moment' ], factory);
 	}
 	else {
 		factory(jQuery, moment);
