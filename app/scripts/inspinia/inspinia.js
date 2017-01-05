634023e69e2758e780a96522fb832b40703f1b22
Fredrik Wollsén <fredrik@neam.se>
2017-01-05 12:02:49 +0200

    Updated inspinia controllers, directives and custom js

--- app/scripts/inspinia/inspinia.js
+++ app/scripts/inspinia/inspinia.js
@@ -1,36 +1,67 @@
 /**
  * INSPINIA - Responsive Admin Theme
- * Copyright 2014 Webapplayers.com
+ * 2.6.2
  *
  * Custom scripts
  */
 
 $(document).ready(function () {
 
-    fix_height();
+    // Full height of sidebar
+    function fix_height() {
+        var heightWithoutNavbar = $("body > #wrapper").height() - 61;
+        $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
 
-});
+        var navbarHeigh = $('nav.navbar-default').height();
+        var wrapperHeigh = $('#page-wrapper').height();
 
-// Full height of sidebar
-function fix_height() {
-    var heightWithoutNavbar = $("body > #wrapper").height() - 61;
-    $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
-    var windowHeight = $( window ).height();
-    $("#page-wrapper").css("min-height", windowHeight + "px");
-};
+        if(navbarHeigh > wrapperHeigh){
+            $('#page-wrapper').css("min-height", navbarHeigh + "px");
+        }
 
-$(window).bind("load resize click scroll", function() {
-    if(!$("body").hasClass('body-small')) {
-        fix_height();
+        if(navbarHeigh < wrapperHeigh){
+            $('#page-wrapper').css("min-height", $(window).height()  + "px");
+        }
+
+        if ($('body').hasClass('fixed-nav')) {
+            if (navbarHeigh > wrapperHeigh) {
+                $('#page-wrapper').css("min-height", navbarHeigh + "px");
+            } else {
+                $('#page-wrapper').css("min-height", $(window).height() - 60 + "px");
+            }
+        }
+
     }
+
+
+    $(window).bind("load resize scroll", function() {
+        if(!$("body").hasClass('body-small')) {
+            fix_height();
+        }
+    });
+
+    // Move right sidebar top after scroll
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav') ) {
+            $('#right-sidebar').addClass('sidebar-top');
+        } else {
+            $('#right-sidebar').removeClass('sidebar-top');
+        }
+    });
+
+
+    setTimeout(function(){
+        fix_height();
+    })
 });
 
 // Minimalize menu when screen is less than 768px
-$(window).bind("load resize", function() {
-    if ($(this).width() < 769) {
-        $('body').addClass('body-small')
-    } else {
-        $('body').removeClass('body-small')
-    }
-});
-
+$(function() {
+    $(window).bind("load resize", function() {
+        if ($(document).width() < 769) {
+            $('body').addClass('body-small')
+        } else {
+            $('body').removeClass('body-small')
+        }
+    })
+})
