3e3805c5d3fec0ada2eb66da4586a96c754f3a5e
Fredrik Wollsén <fredrik@neam.se>
2017-01-07 12:15:00 +0200

    Including config from https://github.com/crisu83/webpack-minimal (tweaked) and added workaround to the fact that babel 5->6 upgrade handles exports differently

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,52 @@
-var config = {
-  entry: {
-    app: ['./src/core/bootstrap.js'],
-  },
-  output: {
-    path:     __dirname + '/build/',
-    filename: 'bundle.js',
-  },
-  resolve: {
-    root: __dirname + '/../',
-    extensions: [ '', '.js' ]
-  },
-  module: {
-    noParse: [],
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'ng-annotate!babel' },
-      { test: /\.html$/, loader: 'raw' },
-    ]
-  }
+var path = require('path');
+var webpack = require('webpack');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+
+module.exports = {
+    devtool: 'eval',
+    entry: {
+        app: ['./src/core/bootstrap.js'],
+    },
+    output: {
+        path: __dirname + '/build/',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        root: __dirname + '/../',
+        extensions: ['', '.js']
+    },
+    module: {
+        noParse: [],
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['ng-annotate', 'babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                loader: 'raw'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new DashboardPlugin()
+    ],
+    devServer: {
+        hot: true,
+        inline: true,
+        contentBase: './build/',
+        watchOptions: {poll: 1000, ignored: /node_modules/}
+    }
 };
 
-module.exports = config;
