815b2020422d2d1a4f27b9fdd8e27123bbff140c
Fredrik Wollsén <fredrik@neam.se>
2017-01-09 14:04:08 +0200

    Initial webpack-modularization incl loading bootstrapping angular and including compiled app less-styles

--- app/scripts/inspinia/module.js
+++ app/scripts/inspinia/module.js
@@ -2,15 +2,19 @@
  * INSPINIA - Responsive Admin Theme
  *
  */
-(function () {
-    angular.module('inspinia', [
-        'ui.router',                    // Routing
-        'oc.lazyLoad',                  // ocLazyLoad
-        'ui.bootstrap',                 // Ui Bootstrap
-        'pascalprecht.translate',       // Angular Translate
-        'ngIdle',                       // Idle timer
-        'ngSanitize'                    // ngSanitize
-    ])
-})();
+
+'use strict';
+
+export default require('angular').module('inspinia', [
+    require('angular-ui-router'),                    // Routing
+    (() => {
+        require('oclazyload/dist/ocLazyLoad.js');
+        return 'oc.lazyLoad'
+    })(),                  // ocLazyLoad
+    require('angular-bootstrap'),                 // Ui Bootstrap
+    require('angular-translate'),       // Angular Translate
+    require('ng-idle'),                       // Idle timer
+    require('angular-sanitize')                    // ngSanitize
+]);
 
 // Other libraries are loaded dynamically in the config.js file using the library ocLazyLoad
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var DashboardPlugin = require('webpack-dashboard/plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'eval',
@@ -8,16 +10,16 @@ module.exports = {
         app: ['./src/core/bootstrap.js'],
     },
     output: {
-        path: __dirname + '/../angular-frontend-dna/app/build/',
+        path: __dirname + '/dist/',
         filename: 'bundle.js',
     },
     resolve: {
         root: __dirname + '/../',
         modulesDirectories: [__dirname + '/../angular-frontend/node_modules', 'node_modules'],
         alias: {
-            'project': __dirname + '/../angular-frontend-dna/app/',
-            'shared': __dirname + '/app/',
-            'webpack-angular-examplecode': __dirname + '/src/',
+            'project': __dirname + '/../angular-frontend-dna/app',
+            'shared': __dirname + '/app',
+            'webpack-angular-examplecode': __dirname + '/src',
         },
         extensions: ['', '.js']
     },
@@ -45,11 +47,42 @@ module.exports = {
             },
             {
                 test: /\.html$/,
-                loader: 'raw'
+                loader: 'raw-loader',
+                exclude: /index\.html$/
             },
             {
                 test: /\.json$/,
                 loader: 'json-loader'
+            },
+            /*
+             * Extract and compile LESS files to external CSS file
+             */
+            {
+                test: /\.less/,
+                loader: ExtractTextPlugin.extract(
+                    'style-loader',
+                    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!less-loader'
+                ),
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?&name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png)$/,
+                exclude: /node_modules/,
+                loader: 'file?limit=1024&name=images/[name].[ext]'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'
             },
             {
                 test: /\.css$/,
@@ -59,13 +92,18 @@ module.exports = {
         ]
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            template: __dirname + '/../angular-frontend-dna/app/index.html'
+        }),
+        new ExtractTextPlugin('[name].[chunkhash].css' /*, {publicPath: '../'}*/),
         new webpack.HotModuleReplacementPlugin(),
         new DashboardPlugin()
     ],
     devServer: {
         hot: true,
         inline: true,
-        contentBase: __dirname + '/../angular-frontend-dna/app/',
+        contentBase: __dirname + '/dist/',
         watchOptions: {poll: 1000, ignored: /node_modules/}
     }
 };
