ba85e786f25847dfb0fc1da5ac9da018d6ad5441
Fredrik Wollsén <fredrik@neam.se>
2017-02-05 22:08:21 +0200

    View-files related to inspinia updated

--- app/scripts/inspinia/directives.js
+++ app/scripts/inspinia/directives.js
@@ -120,7 +120,7 @@ function iboxTools($timeout) {
     return {
         restrict: 'A',
         scope: true,
-        templateUrl: 'views/common/ibox_tools.html',
+        template: require('views/common/ibox_tools.html'),
         controller: function ($scope, $element) {
             // Function for collapse ibox
             $scope.showhide = function () {
@@ -152,7 +152,7 @@ function iboxToolsFullScreen($timeout) {
     return {
         restrict: 'A',
         scope: true,
-        templateUrl: 'views/common/ibox_tools_full_screen.html',
+        template: require('views/common/ibox_tools_full_screen.html'),
         controller: function ($scope, $element) {
             // Function for collapse ibox
             $scope.showhide = function () {
