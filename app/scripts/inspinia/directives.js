b1cc8683f048d774a68fb93aca5d599694b57ff4
Fredrik Wollsén <fredrik@neam.se>
2017-02-05 22:15:33 +0200

    Made page title directive use the site name env var

--- app/scripts/inspinia/directives.js
+++ app/scripts/inspinia/directives.js
@@ -34,6 +34,7 @@
  *
  */
 
+let env = require('shared/scripts/env');
 
 /**
  * pageTitle - Directive for set Page title - mata title
@@ -43,9 +44,9 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 // Default title - load on Dashboard 1
-                var title = 'INSPINIA | Responsive Admin Theme';
+                var title = env.SITENAME;
                 // Create your own title pattern
-                if (toState.data && toState.data.pageTitle) title = 'INSPINIA | ' + toState.data.pageTitle;
+                if (toState.data && toState.data.pageTitle) title = env.SITENAME + ' | ' + toState.data.pageTitle;
                 $timeout(function() {
                     element.text(title);
                 });
